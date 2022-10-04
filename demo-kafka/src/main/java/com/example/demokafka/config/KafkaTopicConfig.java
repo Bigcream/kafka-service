2feb52933561af2b59e1819e6e280560f8de4dd5
Bigcream <96058142+Bigcream@users.noreply.github.com>
2022-10-05 00:42:17 +0700

    test

--- demo-kafka/src/main/java/com/example/demokafka/config/KafkaTopicConfig.java
+++ demo-kafka/src/main/java/com/example/demokafka/config/KafkaTopicConfig.java
@@ -3,21 +3,9 @@ package com.example.demokafka.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
+
 
 @Configuration
 public class KafkaTopicConfig {
 
-    @Bean
-    public NewTopic demoTopic(){
-        return TopicBuilder.name("demoTopic").build();
-    }
-
-    @Bean
-    public NewTopic demoTopicJson(){
-        return TopicBuilder.name("demoTopicJson")
-                .partitions(3)
-                .replicas(3)
-                .build();
-    }
 }
--- demo-kafka/src/main/java/com/example/demokafka/controller/MessageController.java
+++ demo-kafka/src/main/java/com/example/demokafka/controller/MessageController.java
@@ -4,12 +4,16 @@ import com.example.demokafka.kafka.JsonKafkaProducer;
 import com.example.demokafka.kafka.KafkaProducer;
 import com.example.demokafka.payload.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/kafka")
 public class MessageController {
+
+    @Autowired
+    private RedisTemplate<String, String> template;
 
     @Autowired
     private JsonKafkaProducer jsonKafkaProducer;
@@ -19,8 +23,15 @@ public class MessageController {
 
     @GetMapping("/publish")
     public ResponseEntity<String> publish(@RequestParam("message") String message){
-        KafkaProducer.sendMessage(message);
-        return ResponseEntity.ok("Message sent topic");
+        User user = new User();
+        user.setId(1);
+        user.setFirstName("abc");
+        user.setLastName("123");
+        template.opsForValue().set("loda", String.valueOf(user));
+
+        // In ra màn hình Giá trị của key "loda" trong Redis
+        System.out.println("Value of key loda: "+template.opsForValue().get("loda"));
+        return ResponseEntity.ok("Message sent topic" + String.valueOf(user));
     }
 
     @PostMapping("/publish-json")
