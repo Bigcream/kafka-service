09bd501009307d83e878730de5b81475263dc2ca
Bigcream <96058142+Bigcream@users.noreply.github.com>
2023-01-04 22:35:35 +0700

    config nexus repo maven

---
+++ src/main/java/com/kafkaservice/config/KafkaTopicConfig.java
@@ -0,0 +1,23 @@
+package com.kafkaservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic demoTopic(){
+        return TopicBuilder.name("demoTopic").build();
+    }
+
+    @Bean
+    public NewTopic demoTopicJson(){
+        return TopicBuilder.name("demoTopicJson")
+                .partitions(3)
+                .replicas(3)
+                .build();
+    }
+}
